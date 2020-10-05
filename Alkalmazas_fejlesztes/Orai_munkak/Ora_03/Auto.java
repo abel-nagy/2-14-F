716f2c4d52ae39ee3101728a6bdff80a11c96fc2
Abel Nagy <sens1tiv@hotmail.com>
2020-10-05 17:24:52 +0200

    Create Auto.java

---
+++ Alkalmazas_fejlesztes/Orai_munkak/Ora_03/Auto.java
@@ -0,0 +1,28 @@
+
+public class Auto {
+	
+	/*= Variables =======================================================================*/
+	
+	int id;
+	String rendszam;
+	String tipus;
+	String szin;
+	int hengerurtartalom;
+	int benzinese;
+	int forgalmi;
+	int ar;
+	
+	/*= Constructor =====================================================================*/
+	
+	public Auto(int id, String rendszam, String tipus, String szin, int hengerurtartalom, int benzinese, int forgalmi, int ar) {
+		this.id = id;
+		this.rendszam = rendszam;
+		this.tipus = tipus;
+		this.szin = szin;
+		this.hengerurtartalom = hengerurtartalom;
+		this.benzinese = benzinese;
+		this.forgalmi = forgalmi;
+		this.ar = ar;
+	}
+
+}
