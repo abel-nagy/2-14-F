e30b5ad34e108730eca34608cf3ff5da45927fdd
sens1tiv <sens1tiv@hotmail.com>
2020-10-05 15:38:23 +0200

    WEB Orai munka04 frissítve, szépítve

--- pages/Webfejlesztes/Orai_munkak/Ora_4/script.js
+++ pages/Webfejlesztes/Orai_munkak/Ora_4/script.js
@@ -1,29 +1,30 @@
 window.addEventListener("load", init);
 
 function init() {
-    document.getElementById("gomb").addEventListener('click', szamol);
+    document.getElementById('btn').addEventListener('click', szamol);
 }
 
 function szamol() {
-    let alap = parseInt( document.getElementById("alap").value );
-    let kamat = parseInt( document.getElementById("kamat").value );
-    let ev = parseInt( document.getElementById("ev").value );
+    const alap = parseInt( document.getElementById("alap").value );
+    const kamat = parseInt( document.getElementById("kamat").value );
+    const ev = parseInt( document.getElementById("ev").value );
 
     console.log(alap + ", " + kamat + ", " + ev);
 
-    let tomb = new Array(ev).fill(0)
+    const tomb = new Array(ev).fill(0);
 
     for(i in tomb) {
-        tomb[i] = i > 0 ? Math.round( tomb[i - 1] + tomb[i - 1] * kamat/100 ) : alap;
+        tomb[i] = i > 0 ? tomb[i - 1] + Math.round(tomb[i - 1] * kamat / 100) : alap;
     }
 
     console.log(tomb);
 
-    kiiras(tomb);
+    document.getElementById('tbl').innerHTML = kiiras(tomb);
 
 }
 
 function kiiras(arr) {
-    let sorok = arr.reduce( (s, e, i) => s + `<tr><td>${i + 1}</td><td>` );
-    document.getElementById("tbl").innerHTML = sorok;
+    fejlec = "<thead class='thead-dark'><tr><th scope='col'>Év</th><th scope='col'>Összeg</th></tr></thead><tbody>";
+    const cucc = arr.reduce( (sorok, e, i) => sorok + '<tr><td>' + (i + 1) + '</td><td>' + e + '</td></tr>', fejlec);
+	return cucc+"</tbody>";
 }
