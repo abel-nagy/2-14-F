9711bd5cb0d11a8fda50c15bd2481ac891661d42
Abel Nagy <sens1tiv@hotmail.com>
2020-10-08 08:42:10 +0200

    Kód Szépítés

--- pages/Webfejlesztes/Orai_munkak/Ora_4/script.js
+++ pages/Webfejlesztes/Orai_munkak/Ora_4/script.js
@@ -1,30 +1,35 @@
 window.addEventListener("load", init);
 
 function init() {
-    document.getElementById('btn').addEventListener('click', szamol);
+  document.getElementById("btn").addEventListener("click", szamol);
 }
 
 function szamol() {
-    const alap = parseInt( document.getElementById("alap").value );
-    const kamat = parseInt( document.getElementById("kamat").value );
-    const ev = parseInt( document.getElementById("ev").value );
+  const alap = parseInt(document.getElementById("alap").value);
+  const kamat = parseInt(document.getElementById("kamat").value);
+  const ev = parseInt(document.getElementById("ev").value);
 
-    console.log(alap + ", " + kamat + ", " + ev);
+  console.log(alap + ", " + kamat + ", " + ev);
 
-    const tomb = new Array(ev).fill(0);
+  const tomb = new Array(ev).fill(0);
 
-    for(i in tomb) {
-        tomb[i] = i > 0 ? tomb[i - 1] + Math.round(tomb[i - 1] * kamat / 100) : alap;
-    }
+  for (i in tomb) {
+    tomb[i] =
+      i > 0 ? tomb[i - 1] + Math.round((tomb[i - 1] * kamat) / 100) : alap;
+  }
 
-    console.log(tomb);
+  console.log(tomb);
 
-    document.getElementById('tbl').innerHTML = kiiras(tomb);
-
+  document.getElementById("tbl").innerHTML = kiiras(tomb);
 }
 
 function kiiras(arr) {
-    fejlec = "<thead class='thead-dark'><tr><th scope='col'>Év</th><th scope='col'>Összeg</th></tr></thead><tbody>";
-    const cucc = arr.reduce( (sorok, e, i) => sorok + '<tr><td>' + (i + 1) + '</td><td>' + e + '</td></tr>', fejlec);
-	return cucc+"</tbody>";
+  fejlec =
+    "<thead class='thead-dark'><tr><th scope='col'>Év</th><th scope='col'>Összeg</th></tr></thead><tbody>";
+  const cucc = arr.reduce(
+    (sorok, e, i) =>
+      sorok + "<tr><td>" + (i + 1) + "</td><td>" + e + "</td></tr>",
+    fejlec
+  );
+  return cucc + "</tbody>";
 }
